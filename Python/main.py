bcfb040c88e434963fd9a56a8b27a837dc025a5e
James Key <100178397+EternalBlue3@users.noreply.github.com>
2023-07-03 17:56:24 -0400

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,5 @@
 from PurposeGames import get_quiz_info, submit_quiz_results
-import time, random, requests
+import time, random, requests, sys
 from bs4 import BeautifulSoup
 
 def obfuscate_results(url):    
@@ -17,17 +17,7 @@ def obfuscate_results(url):
     print(f"Estimated reasonable submission time: {finaltime}")
     print(f"Quiz Obfuscation for: {url}")
     
-    if num_questions < 3:
-        print("Correct Guesses: 1")
-        
-        fakeacc = str(round((1 / num_questions) * 100)) # Automatically assume 1 correct answer
-        faketime = (15 + random.randint(1,20)) * 10
-        
-        print(f"Time: {faketime}")
-        print(f"Accuracy: {fakeacc}\n")
-        submit_quiz_results(url, faketime, fakeacc, gameid, tid, token, nonce, csrf)
-        time.sleep(60)
-    
+    # If the number of questions is less than 3, don't bother faking a playthrough with less than 100% accuracy because the server will most likely not even save that playthrough in the stats section
     if num_questions >= 3 and num_questions < 11:
         correct_guesses = random.randint(1, num_questions-2)
         print(f"Correct Guesses: {correct_guesses}")
@@ -38,23 +28,22 @@ def obfuscate_results(url):
         print(f"Time: {faketime}")
         print(f"Accuracy: {fakeacc}\n")
         submit_quiz_results(url, faketime, fakeacc, gameid, tid, token, nonce, csrf)
-        time.sleep(60)
+        time.sleep(30)
     
     if num_questions >= 11 and num_questions < 25:
-        for x in range(2):
-            correct_guesses = random.randint(1,num_questions-5) if x == 0 else correct_guesses + random.randint(2,4)
-            print(f"Correct Guesses: {correct_guesses}")
-            
-            fakeacc = str(round((correct_guesses / num_questions) * 100))
-            faketime = (35 + random.randint(1,30) - (random.randint(1,15) * x)) * 10
-            
-            print(f"Time: {faketime}")
-            print(f"Accuracy: {fakeacc}\n")
-            submit_quiz_results(url, faketime, fakeacc, gameid, tid, token, nonce, csrf)
-            time.sleep(60)
+        correct_guesses = random.randint(1,num_questions-5)
+        print(f"Correct Guesses: {correct_guesses}")
+
+        fakeacc = str(round((correct_guesses / num_questions) * 100))
+        faketime = (35 + random.randint(1,30)) * 10
+
+        print(f"Time: {faketime}")
+        print(f"Accuracy: {fakeacc}\n")
+        submit_quiz_results(url, faketime, fakeacc, gameid, tid, token, nonce, csrf)
+        time.sleep(30)
             
     if num_questions >= 25:
-        for x in range(3):
+        for x in range(2):
             correct_guesses = random.randint(1,num_questions-10) if x == 0 else correct_guesses + random.randint(2,4)
             print(f"Correct Guesses: {correct_guesses}")
             
@@ -64,32 +53,28 @@ def obfuscate_results(url):
             print(f"Time: {faketime}")
             print(f"Accuracy: {fakeacc}\n")
             submit_quiz_results(url, faketime, fakeacc, gameid, tid, token, nonce, csrf)
-            time.sleep(60)
+            time.sleep(30)
             
     # Submit final quiz results
     print(f"Finalizing time of {str(finaltime)}.\n\n")
     submit_quiz_results(url, finaltime, "100", gameid, tid, token, nonce, csrf)
-    time.sleep(60)
 
 if __name__ == '__main__':
-    print("Webscraping...")
-    links = []
-
-    for x in range(10):  # Find all quizzes on the first 10 pages
-        success = False
-        while not success:
-            try:
-                html_content = requests.get(f"https://www.purposegames.com/games?sort=latest&page={str(x)}").text
-                success = True
-            except Exception as e:
-                print(f"Entountered error: {e}")
-
-        soup = BeautifulSoup(html_content, "html.parser")
-
-        # Find all games on the page and get the href values
-        titles = soup.find_all(class_="game-title")
-        links += [title.get("href") for title in titles][:50]
-        
-    print("Finished webscraping. Starting obfuscated score submissions...\n\n\n")
-    for x in links:
-        obfuscate_results(x)
+    print("Reading Quizzes...")
+    with open("quizzes.txt","r") as fh:
+        links = [link.strip() for link in fh.readlines()]
+    
+    print("Finished reading file. Starting obfuscated score submissions...\n\n\n")
+    quizzes_played = 0
+    for quiz in links:
+        try:
+            obfuscate_results(quiz)
+            quizzes_played += 1
+            time.sleep(30) # Add delay between playing quizzes
+        except KeyboardInterrupt:
+            # Remove quizzes the program has already played on exit
+            links = links[quizzes_played:]
+            with open("quizzes.txt","w") as fh:
+                fh.write('\n'.join(links))
+            print("Exiting...")
+            sys.exit()
